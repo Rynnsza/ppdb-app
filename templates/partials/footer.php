3f14250a68b5b3cb358d3fdec8573104af4e58e8
Ahmad Ambari <ahmadambari044@gmail.com>
2026-04-30 22:57:44 +0700

    first commit

---
+++ templates/partials/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="container">
+        <p>&copy; <?php echo date("Y"); ?> PPDB Application With Ryz. All rights reserved.</p>
+        <ul class="social-media">
+            <li><a href="#">Facebook</a>https://www.facebook.com/smpn.sukosari</li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Instagram</a>https://www.instagram.com/spasas_jelita/</li>
+        </ul>
+    </div>
+</footer>
+<script src="assets/js/app.js"></script>
